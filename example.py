95e758f6fa03c30c5c8a164775a19991818bf414
Yazdan Ranjbar <yazdan_ra@icloud.com>
2021-09-09 19:51:44 -0600

    Initialize cpu monitor

--- example.py
+++ example.py
@@ -2,13 +2,31 @@ from pymonitor import monitor
 
 
 @monitor("time")
-def example_function():
+def time_example():
     from time import sleep
 
     sleep(7)
 
-    pass
+
+@monitor("time")
+@monitor("cpu")
+def cpu_example():
+    import random
+    from time import sleep
+
+    random_list = list()
+    for _ in range(10000000):
+        random_list.append(random.randint(0, 1000000000))
+    sorted(random_list)
+
+    sleep(30)
+
+    random_list = list()
+    for _ in range(10000000):
+        random_list.append(random.randint(0, 1000000000))
+    sorted(random_list)
 
 
 if __name__ == "__main__":
-    example_function()
+    # time_example()
+    cpu_example()
--- pymonitor/decorators.py
+++ pymonitor/decorators.py
@@ -1,5 +1,9 @@
 import functools
+import resource
+from matplotlib import pyplot as pd
+from threading import Thread
 from datetime import datetime
+from time import sleep
 
 from .templates import time_result
 
@@ -20,8 +24,38 @@ def monitor(measure):
 
         return wrapper
 
+    def cpu_monitor(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            thread = Thread(target=func)
+            thread.start()
+
+            # TODO: override if provided
+            refresh_period = 0.1  # Seconds
+
+            cpu_usage = dict()
+            while thread.is_alive():
+                sleep(refresh_period)
+                cpu_usage[datetime.now()] = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+            print("======================================")
+            for time, usage in cpu_usage.items():
+                print(time.time(), ":  ", usage)
+
+            pd.plot(range(len(cpu_usage)), cpu_usage.values())
+
+            try:
+                pd.savefig("figures/figure.png")
+            except FileNotFoundError:
+                import os
+                os.mkdir("figures")
+                pd.savefig("figures/figure.png")
+
+        return wrapper
+
     monitor_map = dict(
         time=time_monitor,
+        cpu=cpu_monitor,
     )
 
     if measure not in monitor_map.keys():
